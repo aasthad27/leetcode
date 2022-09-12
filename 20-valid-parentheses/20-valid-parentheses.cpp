af309efd9b5660ee65a32083b35ae7dd8274f49c
Aastha Dubey <aasthad27@gmail.com>
2022-09-12 23:51:33 +0530

    Time: 3 ms (46.96%), Space: 6.3 MB (51.00%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n=s.length();
+            stack<char>st;
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='(' || s[i]=='{'||s[i]=='[')
+                st.push(s[i]);
+            else
+            {
+                if(st.empty())
+                    return false;
+            char c=st.top();
+            st.pop();
+            if((s[i]==')' && c=='(' )||(s[i]=='}' && c=='{' )||(s[i]==']' && c=='[')) 
+              continue;
+            else
+                return false;
+            }
+            
+        }
+        if(st.empty())
+            return true;
+        return false;
+    }
+        };
