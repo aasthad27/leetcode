92aecd08ed8439ea154acfa36b837d3f14e91b2f
Aastha Dubey <aasthad27@gmail.com>
2022-09-17 10:36:03 +0530

    Time: 27 ms (85.24%), Space: 19.1 MB (83.37%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+int count=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]!=0)
+            { 
+            nums[count]=nums[i];
+            count++;
+            }
+            
+        }
+        while(count<n)
+        {
+            nums[count]=0;
+            count++;
+        }
+        
+    }
+};
