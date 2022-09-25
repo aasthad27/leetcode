4a2d6cd57baad18ea0b7d0929efc36a164131be5
Aastha Dubey <aasthad27@gmail.com>
2022-09-25 23:49:51 +0530

    Time: 10 ms (6.89%), Space: 6.6 MB (10.65%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i=s.length()-1;
+        int c=0;
+        while(i>=0 && s[i]==' ')
+        {
+            i--;
+        }
+       while(i>=0 && s[i]!=' ')
+       {
+           i--;
+           c++;
+       }
+        return c;
+    }
+};
