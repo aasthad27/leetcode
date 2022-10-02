2b7a0bd79675673b757f4f83eb6a0e9ef186d6ec
Aastha Dubey <aasthad27@gmail.com>
2022-10-02 23:54:35 +0530

    Time: 10 ms (73.74%), Space: 15.1 MB (8.15%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,38 +11,38 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode *ans=new ListNode();
-        ListNode *head=ans;
-        while(list1&& list2)
+        ListNode *ans=new ListNode(); // create a new list as ans 
+        ListNode *head=ans; // head pointing to the address of ans 
+        while(list1&& list2) 
         {
-            if( list1->val <= list2->val)
+            if( list1->val <= list2->val) // if elements of list one is small then print list 1
             {
                 
                 ans->next=new ListNode(list1->val);
-                list1=list1->next;
+                list1=list1->next; // list 1 next element check
         
             }
-            else
+            else                    // if elements of list two is small print list  2 
             {
               ans->next=new ListNode(list2->val);
-                list2=list2->next;
+                list2=list2->next;            // list 2 next element check
                 
             }
-            ans=ans->next;
+            ans=ans->next;   // ans ko next kar do
         }
-        while(list1!=NULL)
+        while(list1!=NULL) // if list 1 doesn't end then print rest elemnets in list 1
         {
             ans->next=new ListNode(list1->val);
-            list1=list1->next;
-            ans=ans->next;
+            list1=list1->next; // list 1 ko aage 
+            ans=ans->next; // ans ko aage 
 }
-        while(list2!=NULL)
+        while(list2!=NULL) //  same for list 2
         {
             ans->next=new ListNode(list2->val);
             list2=list2->next;
             ans=ans->next;
 }
-        return head->next;
+        return head->next; // head next cuz ans next se elements store hue hai :)))))
         
     }
 };
