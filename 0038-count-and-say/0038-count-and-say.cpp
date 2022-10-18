300dfb299ebc8ad57ce3be901901bae0753eab26
Aastha Dubey <aasthad27@gmail.com>
2022-10-18 23:09:08 +0530

    Time: 150 ms (6.07%), Space: 121.2 MB (15.35%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string countAndSay(int n) {
-    string ans="1";
+    string ans="1"; // for n=1 it will be 1 always 
         for(int i=2;i<=n;i++)
         {
             string s=ans;
@@ -10,12 +10,12 @@ public:
             while(j<s.length())
             {
                 int k=j;
-                while(j<s.length() && s[j]==s[k])
+                while(j<s.length() && s[j]==s[k]) //check for repeated 
                 {
-                    k++;
+                    k++;                         //stores the count
                 }
-                ans =ans + to_string(k-j)+s[j];
-                j=k;
+                ans =ans + to_string(k-j)+s[j]; //append count and that element
+                j=k; // move to the non repeating for next iteration
             }
         }
         return ans;
