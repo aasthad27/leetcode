7a1b1df202e2c138baf82884cc821f496fccf53a
Aastha Dubey <aasthad27@gmail.com>
2023-04-07 00:10:01 +0530

    Time: 152 ms (56.49%), Space: 71.4 MB (28.67%) - LeetHub

---
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool arrayIsPossibleForY(vector<int>& nums,int mid)
+    { 
+        int n=nums.size();
+        long sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=nums[i];
+            if(sum > (long) mid*(i+1))
+                return false;
+          
+        }
+        return true;
+    }
+    int minimizeArrayValue(vector<int>& nums) {
+        int maxi=INT_MIN;
+        int l=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            maxi=max(maxi,nums[i]);
+        }
+        int r=maxi;
+        int ans=0;
+        while(l<=r)
+        {
+            int mid=l+(r-l)/2;
+            if(arrayIsPossibleForY(nums,mid)) // valid
+            {
+                ans=mid;                             // check left -->for smaller number 
+                r=mid-1;
+            }
+            else
+            {
+                l=mid+1;
+            }
+        }
+        return ans;
+    }
+};
